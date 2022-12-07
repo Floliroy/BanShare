1ce93adff125934344d96ab910ff64ac505d6c9e
Flo Leeroy <florian.leeroy@hotmail.com>
2022-12-07 11:06:24 +0100

    real fix

--- services/Users.js
+++ services/Users.js
@@ -136,7 +136,6 @@ module.exports = class Users{
             users.sort(function(a, b){
                 return getSubs(b.id, users) - getSubs(a.id, users)
             })
-            console.log(users)
             return users
         }catch (error){
             return new Array()
