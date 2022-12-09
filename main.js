c808a04c153220801a6e55fc925e29b46ebccc03
Flo Leeroy <florian.leeroy@hotmail.com>
2022-12-10 00:36:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,6 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
-app.use(express.json())
 app.use(cookieParser())
 
 /**
